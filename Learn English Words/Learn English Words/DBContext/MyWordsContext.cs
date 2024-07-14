1ed2470f376750636bf69ae653529a06822f382a
Andriy Sozanskiy <sozanskiyandriyw@gmail.com>
2024-07-14 03:34:46 +0300

    Configure Entity Framework Core

---
+++ Learn English Words/Learn English Words/DBContext/MyWordsContext.cs
@@ -0,0 +1,78 @@
+﻿using Learn_English_Words.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learn_English_Words.DBContext
+{
+    internal class MyWordsContext : DbContext
+    {
+        private static IConfigurationRoot configuration;
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                if (configuration == null)
+                {
+                    // Встановлення каталогу для пошуку appsettings.json
+                    configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+                }
+
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure Word entity
+            modelBuilder.Entity<Word>(entity =>
+            {
+                entity.Property(w => w.EnglishWord)
+                      .HasMaxLength(50)
+                      .IsRequired();
+
+                entity.Property(w => w.TranslateWord)
+                      .HasMaxLength(50)
+                      .IsRequired();
+
+                entity.Property(w => w.Description)
+                      .HasMaxLength(255);
+
+                // Define the relationship with Chapter
+                entity.HasOne(w => w.Chapter)
+                      .WithMany(c => c.Words)
+                      .HasForeignKey(w => w.NameChapter)
+                      .HasPrincipalKey(c => c.NameChapter);
+            });
+
+            // Configure Chapter entity
+            modelBuilder.Entity<Chapter>(entity =>
+            {
+                entity.Property(c => c.NameChapter)
+                      .HasMaxLength(30);
+
+                entity.Property(c => c.Description)
+                      .HasMaxLength(255);
+
+                entity.HasKey(c => c.NameChapter);
+            });
+
+        }
+    }
+}
---
+++ Learn English Words/Learn English Words/Migrations/20240714003122_WordAndChapterConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Learn_English_Words.Migrations
+{
+    /// <inheritdoc />
+    public partial class WordAndChapterConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chapters",
+                columns: table => new
+                {
+                    NameChapter = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapters", x => x.NameChapter);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnglishWord = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TranslateWord = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    NameChapter = table.Column<string>(type: "nvarchar(30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Words_Chapters_NameChapter",
+                        column: x => x.NameChapter,
+                        principalTable: "Chapters",
+                        principalColumn: "NameChapter",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_NameChapter",
+                table: "Words",
+                column: "NameChapter");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+
+            migrationBuilder.DropTable(
+                name: "Chapters");
+        }
+    }
+}
