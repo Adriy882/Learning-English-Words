eb96f44483a393b81b296db7636c2bd597b2840d
Andriy Sozanskiy <sozanskiyandriyw@gmail.com>
2024-07-14 03:28:35 +0300

    Add model classes

---
+++ Learn English Words/Learn English Words/Model/Chapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_English_Words.Models
+{
+    class Chapter
+    {
+        public string NameChapter { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Word> Words { get; set; } // Навігаційна властивість
+    }
+
+}
---
+++ Learn English Words/Learn English Words/Model/Word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_English_Words.Models
+{
+    internal class Word
+    {
+        public int Id { get; set; }
+        public string EnglishWord { get; set; }
+        public string TranslateWord { get; set; }
+        public string? Description { get; set; }
+
+        public string NameChapter { get; set; } // Зовнішній ключ
+
+        public Chapter Chapter { get; set; }
+    }
+
+}
